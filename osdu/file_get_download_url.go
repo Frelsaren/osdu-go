5fdeb83ae8190f9625bebd9c87d6e76fa4292995
Frelsaren <markus.roiland00@gmail.com>
2025-05-06 12:51:01 +0200

    wip file service

--- osdu/file_get_download_url.go
+++ osdu/file_get_download_url.go
@@ -1,1 +1,27 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+)
+
+type downloadUrlResponse struct {
+	SignedUrl string `json:"SignedUrl"`
+}
+
+func (s *FileService) GetDownloadURL(ctx context.Context, id, expiryTime string) (string, error) {
+	params := make(map[string]string)
+	params["expiryTime"] = expiryTime
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("%s/files/%s/downloadURL", fileServicePath, id), nil, &params)
+	if err != nil {
+		return "", err
+	}
+
+	var resp downloadUrlResponse
+	_, err = s.client.Do(ctx, req, &resp)
+	if err != nil {
+		return "", err
+	}
+
+	return resp.SignedUrl, nil
+}
--- osdu/file_get_upload_url.go
+++ osdu/file_get_upload_url.go
@@ -1,1 +1,26 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+)
+
+type GetUploadURLResponse struct {
+	FileID   string            `json:"FileID"`
+	Location map[string]string `json:"Location"`
+}
+
+func (s *FileService) GetUploadURL(ctx context.Context, expiryTime string) (GetUploadURLResponse, error) {
+	var resp GetUploadURLResponse
+
+	params := make(map[string]string)
+	params["expiryTime"] = expiryTime
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("%s/files/uploadURL", fileServicePath), nil, &params)
+	if err != nil {
+		return resp, err
+	}
+
+	_, err = s.client.Do(ctx, req, &resp)
+
+	return resp, err
+}
