b70239607ba5a25c0009d290c487f52a60074e8a
Frelsaren <markus.roiland00@gmail.com>
2025-05-06 13:04:11 +0200

    cleanup

--- osdu/storage_get_record.go
+++ osdu/storage_get_record.go
@@ -3,19 +3,17 @@ package osdu
 import (
 	"context"
 	"fmt"
-	"net/url"
 	"strings"
 )
 
 func (s *StorageService) GetRecord(ctx context.Context, id string, v *Record, attributes []string) error {
-	url, _ := url.Parse(fmt.Sprintf("%s/records/%s", storageServicePath, id))
 	params := make(map[string]string)
 
 	if len(attributes) > 0 {
 		params["attributes"] = strings.Join(attributes, ",")
 	}
 
-	req, err := s.client.NewRequest("GET", url.String(), nil, &params)
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("%s/records/%s", storageServicePath, id), nil, &params)
 	if err != nil {
 		return err
 	}
