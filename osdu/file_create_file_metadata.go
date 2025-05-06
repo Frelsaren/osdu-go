63db0040297a552110cf99eb9ce6cd7f0f9377cc
Frelsaren <markus.roiland00@gmail.com>
2025-05-06 13:12:04 +0200

    add file metadata endpoints

--- osdu/file_create_file_metadata.go
+++ osdu/file_create_file_metadata.go
@@ -1,1 +1,24 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+)
+
+type createFileMetadataResponse struct {
+	Id string `json:"id"`
+}
+
+func (s *FileService) CreateFileMetadata(ctx context.Context, id string, v *Record) (string, error) {
+	req, err := s.client.NewRequest("DELETE", fmt.Sprintf("%s/files/metadata", fileServicePath), &v, nil)
+	if err != nil {
+		return "", err
+	}
+
+	var response createFileMetadataResponse
+	_, err = s.client.Do(ctx, req, &response)
+	if err != nil {
+		return "", err
+	}
+	return response.Id, err
+}
--- osdu/file_get_file_metadata.go
+++ osdu/file_get_file_metadata.go
@@ -1,1 +1,16 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+)
+
+func (s *FileService) GetFileMetadata(ctx context.Context, id string, v *Record) error {
+	req, err := s.client.NewRequest("GET", fmt.Sprintf("%s/files/%s/metadata", fileServicePath, id), nil, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.client.Do(ctx, req, &v)
+	return err
+}
