f225fd5e42c17ca7ab1a2e4e51b491f61c5ea132
Frelsaren <markus.roiland00@gmail.com>
2025-05-27 21:53:39 +0200

    add tests for entitlement service

--- osdu/entitlement_delete_member_from_group_test.go
+++ osdu/entitlement_delete_member_from_group_test.go
@@ -1,1 +1,26 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestDeleteMemberFromGroup(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+
+	memberEmail := "test.member@test.com"
+	groupEmail := "test.group@test.com"
+	expectedUrl := fmt.Sprintf("/%s/groups/%s/members/%s", entitlementServicePath, groupEmail, memberEmail)
+	mux.HandleFunc(expectedUrl, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+
+		// Return an empty response
+		w.WriteHeader(http.StatusNoContent)
+	})
+	ctx := context.Background()
+	err := client.Entitlement.DeleteMemberFromGroup(ctx, memberEmail, groupEmail)
+	assertNilError(t, err)
+}
