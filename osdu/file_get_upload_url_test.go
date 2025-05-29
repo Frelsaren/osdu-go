433acad66bb5c60e98e7b55303265528e2f66558
Frelsaren <markus.roiland00@gmail.com>
2025-05-29 17:11:24 +0200

    test: add file service tests for GetDownloadURL and GetFileMetadata methods

--- osdu/file_get_upload_url_test.go
+++ osdu/file_get_upload_url_test.go
@@ -1,1 +1,23 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetUploadURL(t *testing.T) {
+	client, mux, _ := setup(t)
+
+	mux.HandleFunc(fmt.Sprintf("/%s/files/uploadURL", fileServicePath), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"FileID": "test.txt", "Location": {}}`)
+	})
+
+	resp, err := client.File.GetUploadURL(context.Background(), "5M")
+	assertNilError(t, err)
+
+	assertNoDiff(t, resp.FileID, "test.txt")
+
+}
