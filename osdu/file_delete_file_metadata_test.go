33ee3c539f9091b796e2583a8a31cb22f2eb0063
Frelsaren <markus.roiland00@gmail.com>
2025-05-29 16:38:53 +0200

    fix: update DeleteFileMetadata method to remove unused parameter and adjust request handling

--- osdu/file_delete_file_metadata_test.go
+++ osdu/file_delete_file_metadata_test.go
@@ -1,1 +1,25 @@
 package osdu
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestDeleteFileMetadata(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+
+	fileID := "file123"
+	mux.HandleFunc(fmt.Sprintf("/%s/files/%s/metadata", fileServicePath, fileID), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	ctx := context.Background()
+
+	err := client.File.DeleteFileMetadata(ctx, fileID)
+
+	assertNilError(t, err)
+}
